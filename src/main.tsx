b8a5b6ac613b1bd5829ad21d765f5bb143f12e0a
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-09-07 15:33:59 +0300

    feat(RegisterUi):adding zod and validations to signup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRouter from '@routes/AppRouter.tsx'
+// styles
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')!).render(
      <AppRouter />
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,11 +1,91 @@
+import { Col, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { SubmitHandler,useForm } from 'react-hook-form';
+import { signUpSchema, signUpType } from '@validations/signUpSchema';
+import { zodResolver } from "@hookform/resolvers/zod";
+import Input from '@components/forms/input/Input';
+
+// import { useNavigate } from 'react-router-dom';
 
 
-function Register() {
-  return (
-    <div>
-      <p>register page</p>
-    </div>
-  )
+
+export default function Register() {
+
+  // const navigate = useNavigate();
+
+      const {
+        register,
+        handleSubmit,
+        formState: { errors },
+
+      } = useForm<signUpType>({
+        mode:"onBlur",
+        resolver:zodResolver(signUpSchema)
+      });
+
+    
+const submitForm:SubmitHandler<signUpType> =(data) =>{
+  console.log(data);
+  
 }
 
-export default Register
+
+  return (
+    <>
+     <Row>
+      <Col md={{span:"6", offset:"3"}}>
+         <Form onSubmit={handleSubmit(submitForm)}>
+
+              <Input 
+              label='First Name'
+              name ="firstName"
+              register={register}
+              error ={errors.firstName?.message}
+              />
+
+              <Input 
+              label='Last Name'
+              name ="lastName"
+              register={register}
+              error ={errors.lastName?.message}
+              />
+              <Input 
+              label='Email Address'
+              name ="email"
+              register={register}
+              error ={errors.email?.message }
+              />
+              <Input 
+              label='Password'
+              name ="password"
+              type='password'
+              register={register}
+              error ={errors.password?.message}
+              />
+              <Input 
+              label='Confirm Password'
+              name ="confirmPassword"
+              type='password'
+              register={register}
+              error ={errors.confirmPassword?.message}
+              />
+
+               <Button 
+                variant="info" 
+                type="submit" 
+                className='text-light' 
+            
+              >
+              Submit
+                
+              </Button>
+          </Form>
+      </Col>
+       
+      </Row>
+    </>
+     
+
+  )
+}
