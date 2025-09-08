5b52739ad1acf9e86226c7fb440f159f681ebc45
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-09-09 01:31:17 +0300

    feat(navbar): add responsive Navbar component with styles and Google Fonts

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,27 @@
-import Footer from '@components/common/Footer/Footer'
-import Header from '@components/common/Header/Header'
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
+import Footer from "@components/common/Footer/Footer";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import styles from "./styles.module.css";
+import Navbar from "@components/Navbar/Navbar";
 const { layoutContainer, wrapper } = styles;
 
 function MainLayout() {
   return (
- <Container className={layoutContainer}>
-      <Header />
+    <Container fluid className={layoutContainer}>
+      <Navbar
+        user={{
+          avatar: "https://i.pravatar.cc/150?img=3",
+          name: "John Doe",
+          email: "john@example.com",
+          type: "Owner",
+        }}
+      />
       <div className={wrapper}>
         <Outlet />
       </div>
       <Footer />
     </Container>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 
-import AppRouter from '@routes/AppRouter.tsx'
+import AppRouter from "@routes/AppRouter.tsx";
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/global.css";
 
-createRoot(document.getElementById('root')!).render(
-     <AppRouter />
-)
+createRoot(document.getElementById("root")!).render(<AppRouter />);
