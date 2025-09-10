d6714308d4a2a6e11e6cbdbdb9b43cf4ebaa4fd8
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-09-10 12:21:40 +0300

    (feat):login successfully with a user in backend

---
+++ src/store/Auth/Act/ActSignIn.ts
@@ -0,0 +1,26 @@
+import { signInType } from '@validations/signInSchema';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "@services/axios-global";
+import { isAxiosError } from "axios";
+
+const ActSignIn =createAsyncThunk('Auth/SignIn',
+    async(formData:signInType,thunkApi)=>{
+
+        
+        const {rejectWithValue,fulfillWithValue}= thunkApi;
+        try {
+            const response = await api.post('/login',formData);
+            console.log(response);
+            
+            return fulfillWithValue(response.data)
+
+        } catch (error) {
+            console.error("SignIn error:", error);
+            if (isAxiosError(error)) {
+                return rejectWithValue(error.response?.data.message || error.message);
+            } else {
+                return rejectWithValue("An unexpected error");
+            }
+        }
+})
+export default ActSignIn
