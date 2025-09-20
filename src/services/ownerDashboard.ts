e5d274ef10454888ffa7bc99d29da37f2255fe3e
Yara Mohamed <myara1405@gmail.com>
2025-09-20 13:24:23 +0300

    feat(ai): connect review analysis & auto description to UI and display chatbot

--- src/services/ownerDashboard.ts
+++ src/services/ownerDashboard.ts
@@ -1,4 +1,5 @@
 import api from "./axios-global"; 
+import { ReviewAnalysisResponse } from "../types/ReviewAnalysis";
 
 // Get Dashboard data
 export const fetchOwnerDashboard = async () => {
@@ -32,5 +33,19 @@ export const updateProperty = async (propertyId: string, formData: FormData) => 
 // Delete Property
 export const deleteProperty = async (propertyId: number) =>{
   const response = await api.delete(`/properties/${propertyId}`);
+  return response.data;
+}
+// Generate Property Description using AI
+export const generateDescription = async (property: {title:string, bedrooms:number,bathrooms:number, size:number, location:string}) => {
+  const res = await api.post('/properties/generate-description', property);
+  
+  if (res.data.success && res.data.description) {
+    return res.data.description;
+  }
+  
+  throw new Error(res.data.message || 'Failed to generate description');
+};
+export async function getPropertyReviewAnalysis(propertyId: number): Promise<ReviewAnalysisResponse>{
+  const response = await api.get(`/properties/${propertyId}/reviews/analysis`);
   return response.data;
 }
